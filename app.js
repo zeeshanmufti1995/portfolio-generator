4aae122d6522f35009fda71ea7d3400ce18af0cf
zeeshan <mycomputer@9HYNSQ2.hq.target.com>
2022-01-01 10:17:04 -0600

    modifed app.js

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
+
+    const generatePage = (name, github) => {
+        return `
+        <!DOCTYPE html> 
+        <html lang="en"> 
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <title>Portfolio Demo</title>
+        </head>
+      
+        <body>
+          <h1>${name}</h1>
+          <h2><a href="https://github.com/${github}">Github</a></h2>
+        </body>
+        </html>
+        `;
+      };
   
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+
+    fs.writeFile('index.html', generatePage(name, github), err => {
+        if(err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
