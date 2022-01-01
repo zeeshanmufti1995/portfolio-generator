254a8b2131282d43aeb512bfdae672700e1baf2d
zeeshan <mycomputer@9HYNSQ2.hq.target.com>
2022-01-01 07:33:26 -0600

    added app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+    // Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+  };
