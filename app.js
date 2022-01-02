7dd4e0c1708a34d29eecc32e32481baba625513e
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-01-02 10:41:10 -0600

    Added prompt 1st question

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-    const generatePage = (name, github) => {
-        return `
-        <!DOCTYPE html> 
-        <html lang="en"> 
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <title>Portfolio Demo</title>
-        </head>
-      
-        <body>
-          <h1>${name}</h1>
-          <h2><a href="https://github.com/${github}">Github</a></h2>
-        </body>
-        </html>
-        `;
-      };
-  
+// const pageHTML = generatePage(name, github);
 
-    fs.writeFile('index.html', generatePage(name, github), err => {
-        if(err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
