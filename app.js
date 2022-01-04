6d79eaff4768713f1296fa4a790d3a967fa71ea9
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-01-04 11:07:48 -0600

    modefied app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -125,15 +126,21 @@ Add a New Project
       }
     });
 };
-
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-   
-    // will be uncommented in lesson 4
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
